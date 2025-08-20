c02670db12fbcdab7fea219e39c46199a02de162
Jadson <jadsonp.dev@gmail.com>
2025-08-19 22:20:57 -0300

    Creating asset folder  and doing the menu (background, sound and fonts)

--- code/game.py
+++ code/game.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import pygame
-from code.menu import Menu
-from code.const import WIN_HEIGHT, WIN_WIDTH
+from code.Menu import Menu
+from code.Const import WIN_HEIGHT, WIN_WIDTH
 
 class Game:
     def __init__(self):
         pygame.init()
         self.window = pygame.display.set_mode(size =(WIN_WIDTH, WIN_HEIGHT))
 
-
     def run(self):
         while True:
             menu = Menu(self.window)
             menu.run()
-            pass
+    
 
           
--- code/menu.py
+++ code/menu.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import pygame.image
+from pygame import Surface, Rect
+from pygame.font import Font
 
+from code.Const import COLOR_ORANGE, COLOR_WHITE, MENU_OPTION, WIN_WIDTH
 
 
 class Menu:
@@ -16,6 +19,12 @@ class Menu:
         pygame.mixer_music.play(-1)
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(50, 'Mountain', COLOR_ORANGE, ((WIN_WIDTH/2), 70))
+            self.menu_text(50, 'Shooter', COLOR_ORANGE, ((WIN_WIDTH/2), 120))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH/2), 200 + 25 * i))
+
             pygame.display.flip()
 
             #check for all events
@@ -24,4 +33,8 @@ class Menu:
                     pygame.quit() #close Window
                     quit() #end game
 
-    
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
